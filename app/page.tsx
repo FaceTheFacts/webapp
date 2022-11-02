a4ee2723f6fdb8b4c6fd85090a7cbf2286a826b5
Richard <richard.kruemmel@code.berlin>
2022-11-02 16:19:20 +0100

    Add app directory and setup dummy layout and page

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import "../styles/globals.css";
+
+export default function HomePage() {
+  return (
+    <div>
+      <h1>Home Page</h1>
+      <p className="text-red-600">Some content</p>
+    </div>
+  );
+}
