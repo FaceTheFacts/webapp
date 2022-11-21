3482a82511bb677594885f38ad7b0ac3d001de52
Matias Cardone <matias.cardone@axelspringer.com>
2022-11-21 17:42:51 +0100

    Configuration Image-Svg.

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,14 +1,20 @@
+import Image from "next/image";
+import logo from "/public/logo.svg"
 import { Search } from "./Search";
 
 export function Header() {
     return (
     <header>
-        <div> L O G O </div>
-        <div className="bg-black text-slate-200"> Face The Facts Header </div>
-        <Search/>
+        <div>
+            <div>
+                <Image src={logo} height={33} width={35} alt="Face The Facts Logo" />
+                {/* <Logo /> */}
+            </div>
+            <div className="bg-black text-slate-200"> Face The Facts Header </div>
+            <Search/>
+        </div>
     </header>
     )
 }
 
-
-
+// height={33} width={35}
--- next.config.js
+++ next.config.js
@@ -7,6 +7,8 @@ const nextConfig = {
   },
   images: {
     formats: ['image/avif', 'image/webp'],
+    dangerouslyAllowSVG: true,
+    contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
     remotePatterns: [
       {
         protocol: 'https',
