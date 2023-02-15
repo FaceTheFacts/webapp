4f35a924a32b9407b3cd35f8788743298745b73b
Matias Cardone <matias.cardone@axelspringer.com>
2023-02-15 17:14:11 +0100

    Correct location of profile page in the tree.

--- app/profile/[id]/page.tsx
+++ app/profile/[id]/page.tsx
@@ -1,6 +1,34 @@
-// export default function PolitikerId ({ params }) {
-//     const {id} = params
+// Server component
+import Image from 'next/image';
+import { fetchPolitician } from '../../../domain/politician';
 
-//     return <h1> This is the Politikers id = {id} </h1>
-// }
-export{}
+//TO DO: Styling. Resolve use of input refs for the politicians #id
+
+// This page is currently available when navigating to any static #id eg. 139064 , 28899 
+
+export default async function PoliticianPage({ params }) {
+	const  { id }  = params
+	const politiker : Politiker = await fetchPolitician(id);
+  
+  return (
+    <div className="flex flex-row">
+		<Image
+			src={`https://image.facethefacts-api.de/${politiker.id}.jpg`}
+			width={100}
+			height={100}
+			alt={`Bild von ${politiker.label}`}
+      	/>
+        <h1>{politiker.label}</h1>
+		<ul className="p-4 space-y-4 bg-slate-400">
+			<li key={politiker.id}>
+				<ul>
+					{politiker.occupations.map((occupation) => (
+					<li key={politiker.id}> {occupation} </li>
+					))}
+				</ul>
+				<ul key={politiker.party.party_style.id}>{politiker.party.party_style.display_name}</ul>
+			</li>
+		</ul>
+    </div>
+  );
+}
