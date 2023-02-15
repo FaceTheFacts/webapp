7ee2df85a4da86a67053a0a0ac74c22b7a444dde
Matias Cardone <matias.cardone@axelspringer.com>
2023-02-15 17:14:48 +0100

    Create dummy SearchPage.

---
+++ app/search/page.tsx
@@ -0,0 +1,8 @@
+
+export default function SearchPage() {
+  return (
+    <div className='flex h-full flex-col justify-center items-center'>
+      <h1>"Search" page needs a new layout without the Search Component. And the main search bar.</h1>
+    </div>
+  );
+}
