ee20a6c33fabfdf35122d8de6c7ca780710624ef
Richard <richard.kruemmel@code.berlin>
2023-02-16 16:46:59 +0100

    Add bg-color

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import { Search } from "./components/Search";
 
 export default function HomePage() {
   return (
-    <div className="flex h-full flex-col justify-center items-center">
+    <div className="flex h-full flex-col justify-center items-center bg-darkblue">
       <Search />
     </div>
   );
