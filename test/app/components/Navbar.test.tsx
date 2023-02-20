2f2f5ab899ae14c49cb8f1cc6748d782e66ba6f1
Richard <richard.kruemmel@code.berlin>
2023-02-20 20:38:20 +0100

    Add tests for Navbar

---
+++ test/app/components/Navbar.test.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+import "@testing-library/jest-dom";
+import { render } from "@testing-library/react";
+import { Navbar } from "@/app/components/Navbar";
+
+test("renders the Navbar component without errors", () => {
+  render(<Navbar />);
+});
+
+test("displays the logo in the Navbar", () => {
+  const { getByAltText } = render(<Navbar />);
+  const logo = getByAltText("Face The Facts Logo");
+  expect(logo).toBeInTheDocument();
+});
+
+test("displays the correct links in the Navbar", () => {
+  const { getByAltText } = render(<Navbar />);
+  const bundestagLink = getByAltText("Bundestag Logo").parentElement;
+  const searchLink = getByAltText("Search Logo").parentElement;
+  const appPageLink = getByAltText("App Page Logo").parentElement;
+
+  expect(bundestagLink).toHaveAttribute("href", "/bundestag");
+  expect(searchLink).toHaveAttribute("href", "/search");
+  expect(appPageLink).toHaveAttribute("href", "/app-page");
+});
+
+test("displays the correct number of links in the Navbar", () => {
+  const { getAllByRole } = render(<Navbar />);
+  const linkElements = getAllByRole("link");
+  expect(linkElements).toHaveLength(4);
+});
