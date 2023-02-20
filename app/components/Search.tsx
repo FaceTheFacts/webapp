70b6817307af3f6368f739bb9ce6e94a03cbdacb
Matias Cardone <matias.cardone@axelspringer.com>
2023-02-20 14:15:32 +0100

    Connect to search route. Display politician header results.

--- app/components/Search.tsx
+++ app/components/Search.tsx
@@ -1,9 +1,13 @@
 "use client";
 
-import React from "react";
+import React, { useState } from "react";
 import { useForm } from "react-hook-form";
+import SearchResults from "./SearchResults";
+
 interface IFormInputs {
-  searchValue: string;
+  id: number;
+  // postalCode: number;
+  searchValue: string | number;
 }
 
 export function Search() {
@@ -13,10 +17,17 @@ export function Search() {
     // formState: { errors }
   } = useForm<IFormInputs>();
 
+  const [searchResult, setSearchResult] = useState<Search[]> (
+    []
+  )
+
   const onSubmit = async (data: any) => {
-    // console.log(data);
-    const ab = await (await fetch('/api/politician')).json()
-    // console.log(ab)
+    console.log("data", data);
+    // ToDO: type checking fot the searchValue x zipcode or name, and fetch a different route. 
+    // const ab = await (await fetch('/api/politician')).json()
+    const abc = await (await fetch(`http://127.0.0.1:8000/v1/search?text=${data.searchValue}`)).json()
+    console.log(abc)
+    setSearchResult(abc);
   };
 
   return (
@@ -27,6 +38,8 @@ export function Search() {
           </div>
         <button type="submit">CLICK</button>
     </form>
+    <SearchResults results={searchResult} />
+
     </>
   );
 }
