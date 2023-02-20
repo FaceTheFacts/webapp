2d565768c67c85b00400806fd5c900b093f1da1a
Richard <richard.kruemmel@code.berlin>
2023-02-20 20:38:08 +0100

    Fix typescript error

--- app/profile/[id]/page.tsx
+++ app/profile/[id]/page.tsx
@@ -1,35 +1,38 @@
 // Server component
-import Image from 'next/image';
-import { fetchPolitician } from '../../../domain/politician';
-import ProfileImage from '../../components/ProfileImage';
-
+import Image from "next/image";
+import { fetchPolitician } from "../../../domain/politician";
+import ProfileImage from "@/app/components/ProfileImage";
 //TO DO: Styling. Resolve use of input refs for the politicians #id
 
-export default async function PoliticianPage({ params } : { params: { id: number } }) {
-	const  { id }  = params
-	const politician : Politician = await fetchPolitician(id);
-  
-// ToDO:Create image component. Check for img status, when E403 return placeholder image.
+export default async function PoliticianPage({
+  params,
+}: {
+  params: { id: number };
+}) {
+  const { id } = params;
+  const politician: Politician = await fetchPolitician(id);
+
+  // ToDO:Create image component. Check for img status, when E403 return placeholder image.
   return (
     <div className="flex flex-row">
-		{/* <Image
+      {/* <Image
 			src={`https://image.facethefacts-api.de/${politician.id}.jpg`}
 			width={100}
 			height={100}
 			alt={`Bild von ${politician.label}`}
       	/> */}
-		<ProfileImage />
-        <h1>{politician.label}</h1>
-		<ul className="p-4 space-y-4 bg-slate-400">
-			<li>
-				<ul>
-					{politician.occupations.map((occupation, index) => (
-					<li key={index}> {occupation} </li>
-					))}
-				</ul>
-				<ul>{politician.party.party_style.display_name}</ul>
-			</li>
-		</ul>
+      <ProfileImage />
+      <h1>{politician.label}</h1>
+      <ul className="p-4 space-y-4 bg-slate-400">
+        <li>
+          <ul>
+            {politician.occupations.map((occupation, index) => (
+              <li key={index}> {occupation} </li>
+            ))}
+          </ul>
+          <ul>{politician.party.party_style.display_name}</ul>
+        </li>
+      </ul>
     </div>
   );
 }
