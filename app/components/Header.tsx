fd1546d472dbdefb979f471f332708910db78f66
Richard <richard.kruemmel@code.berlin>
2023-02-22 15:48:17 +0100

    Add header styling and logos

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,3 +1,22 @@
+import Image from "next/image";
+import apple from "../../public/assets/apple_logo.png";
+import android from "../../public/assets/android_logo.png";
+import apk from "../../public/assets/apk_logo.png";
+
 export function Header() {
-  return <div className="bg-black text-slate-200">Header</div>;
+  return (
+    <div className="bg-darkblue text-slate-200 flex flex-row justify-end pt-10 pb-4 px-20">
+      <div className="flex">
+        <div className="w-12 h-12 bg-darkgrey rounded-full flex items-center justify-center">
+          <Image height="22" src={apple} alt="Apple Store" />
+        </div>
+        <div className="w-12 h-12 bg-darkgrey rounded-full flex items-center justify-center mx-6">
+          <Image height="22" src={android} alt="Google Play Store" />
+        </div>
+        <div className="w-12 h-12 bg-darkgrey rounded-full flex items-center justify-center">
+          <Image height="12" src={apk} alt=".apk Download" />
+        </div>
+      </div>
+    </div>
+  );
 }
