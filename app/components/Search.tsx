f945535b8de94c350590999c04e214d23b3a6514
Matias Cardone <matias.cardone@axelspringer.com>
2023-03-06 16:56:56 +0100

    Update naming convention.

--- app/components/Search.tsx
+++ app/components/Search.tsx
@@ -24,10 +24,9 @@ export function Search() {
   const onSubmit = async (data: any) => {
     console.log("data", data);
     // ToDO: type checking fot the searchValue x zipcode or name, and fetch a different route. 
-    // const ab = await (await fetch('/api/politician')).json()
-    const abc = await (await fetch(`http://127.0.0.1:8000/v1/search?text=${data.searchValue}`)).json()
-    console.log(abc)
-    setSearchResult(abc);
+    const getSearchValue = await (await fetch(`http://127.0.0.1:8000/v1/search?text=${data.searchValue}`)).json()
+    console.log(getSearchValue)
+    setSearchResult(getSearchValue);
   };
 
   return (
