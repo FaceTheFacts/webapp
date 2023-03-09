233fda76f3c6729843d5fa58c9db908e9e459222
Matias Cardone <matias.cardone@axelspringer.com>
2023-03-09 13:50:30 +0100

    Clean code. Destructuring and naming convention.

--- app/components/Search.tsx
+++ app/components/Search.tsx
@@ -4,40 +4,43 @@ import React, { useState } from "react";
 import { useForm } from "react-hook-form";
 import SearchResults from "./SearchResults";
 
-interface IFormInputs {
+interface SearchInputs {
   id: number;
   // postalCode: number;
   searchValue: string | number;
 }
 
+const API_URL = "http://127.0.0.1:8000/v1/search?text=";
+
 export function Search() {
-  const {
-    register,
-    handleSubmit,
-    // formState: { errors }
-  } = useForm<IFormInputs>();
+  const { register, handleSubmit } = useForm<SearchInputs>();
+  const [searchResult, setSearchResult] = useState<Search[]> ([]);
 
-  const [searchResult, setSearchResult] = useState<Search[]> (
-    []
-  )
-
-  const onSubmit = async (data: any) => {
-    console.log("data", data);
-    // ToDO: type checking for the searchValue x zipcode & name. Provide output for when no seachValue found 404.
-    const getSearchValue = await (await fetch(`http://127.0.0.1:8000/v1/search?text=${data.searchValue}`)).json()
-    console.log(getSearchValue)
-    setSearchResult(getSearchValue);
+  const onSubmit = async (data: SearchInputs) => {
+    try {
+      const response = await fetch(API_URL + data.searchValue);
+      if (!response.ok) {
+        throw new Error("No search results");
+      }
+      const searchValue = await response.json();
+      setSearchResult(searchValue);
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   return (
     <>
     <form onSubmit={handleSubmit(onSubmit)}>
-          <div>
-              <input type="text" {...register("searchValue")}  placeholder="Search..." />
-          </div>
+      <div>
+        <input 
+          type="text" 
+          {...register("searchValue")}  
+          placeholder="Search..." 
+        />
+      </div>
     </form>
-    <SearchResults results={searchResult} />
-
+    <SearchResults searchResults={searchResult} />
     </>
   );
 }
--- app/components/SearchResults.tsx
+++ app/components/SearchResults.tsx
@@ -1,15 +1,22 @@
 import Link from "next/link";
 
-type Props = {
-    results: Search[],
-};
+interface Search {
+    id: number;
+    label: string;
+  }
 
-export default function SearchResults({results}: Props) {
-    return (
-        <ul>
-        {results.map((result) => (
-            <li key={result.id}><Link href={`profile/${result.id}`}>{result.label}</Link></li>
-        ))}    
-        </ul>
-    )
+export default function SearchResults({ searchResults }: { searchResults: Search[] }) {
+    if (searchResults.length === 0) {
+        return <p>No results.</p>
+    }
+
+    const searchResultItems = searchResults.map((result) => (
+        <li key={result.id}>
+            <Link href={`profile/${result.id}`}>
+                {result.label}
+            </Link>
+        </li>
+    ));
+
+    return <ul className='text-white'>{searchResultItems}</ul>;
 }
