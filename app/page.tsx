0589f7a68d981619e49ddf52e658a40c6b4bcfc9
Matias Cardone <matias.cardone@axelspringer.com>
2023-03-10 17:26:37 +0100

    Implemented onSubmit props to succesfully render the Search component.

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,22 @@
+"use client";
+
 import "../styles/globals.css";
 import { Search } from "./components/Search";
 
-export default function HomePage() {
+interface SearchInputs {
+  id: number;
+  // postalCode: number;
+  searchValue: string | number;
+}
+
+export default function HomePage( ) {
+  const handleSubmit = (data: SearchInputs) => {
+    console.log(data);
+  };
+
   return (
     <div className="flex h-full flex-col justify-center items-center bg-darkblue">
-      <Search />
+      <Search onSubmit={handleSubmit}/>
     </div>
   );
 }
