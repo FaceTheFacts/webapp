46b2af208c945b7bcda5b523e49340b1f37d1edc
Matias Cardone <matias.cardone@axelspringer.com>
2023-03-10 17:26:00 +0100

    Modified onSubmit implementation.

--- app/components/Search.tsx
+++ app/components/Search.tsx
@@ -2,21 +2,25 @@
 
 import React, { useState } from "react";
 import { useForm } from "react-hook-form";
-import SearchResults from "./SearchResults";
+import SearchResults from "@/app/components/SearchResults";
 
+interface SearchProps {
+  onSubmit: (data: SearchInputs) => void;
+}
 interface SearchInputs {
   id: number;
   // postalCode: number;
   searchValue: string | number;
 }
 
+
 const API_URL = "http://127.0.0.1:8000/v1/search?text=";
 
-export function Search() {
+export function Search({ onSubmit }: SearchProps) {
   const { register, handleSubmit } = useForm<SearchInputs>();
   const [searchResult, setSearchResult] = useState<Search[]> ([]);
 
-  const onSubmit = async (data: SearchInputs) => {
+  const handleFormSubmit = async (data: SearchInputs) => {
     try {
       const response = await fetch(API_URL + data.searchValue);
       if (!response.ok) {
@@ -24,6 +28,7 @@ export function Search() {
       }
       const searchValue = await response.json();
       setSearchResult(searchValue);
+      onSubmit(data);
     } catch (error) {
       console.log(error);
     }
@@ -31,7 +36,7 @@ export function Search() {
 
   return (
     <>
-    <form onSubmit={handleSubmit(onSubmit)}>
+    <form onSubmit={handleSubmit(handleFormSubmit)}>
       <div>
         <input 
           type="text" 
