839ae8e7a81ecc2e77142d78f77351309e96e265
Richard <richard.kruemmel@code.berlin>
2023-03-15 15:15:10 +0100

    Fix layout error

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,10 +4,11 @@ import { Inter } from "@next/font/google";
 import { Navbar } from "./components/Navbar";
 import { Footer } from "./components/Footer";
 import { Header } from "./components/Header";
+import Head from "next/head";
 
 const inter = Inter({
-  variable: '--font-inter',
-  subsets: ["latin"]
+  variable: "--font-inter",
+  subsets: ["latin"],
 });
 
 export default function RootLayout({
@@ -16,25 +17,25 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html className={inter.variable}>
-      <head>
+    <div className={`flex h-screen ${inter.variable}`}>
+      <Head>
         <title>Face The Facts</title>
         <link rel="icon" href="/favicon.ico" />
-      </head>
-      <body className="flex h-screen">
-        <nav className="sticky top-0 h-screen bg-darkblue w-32 border-r border-solid border-darkblue-20">
-          <div className="flex items-center justify-center">
-            <Navbar />
-          </div>
-        </nav>
-        <div className="flex-1 flex flex-col bg-darkblue">
-          <Header />
-          <main className="flex-1 px-4 py-8 overflow-y-auto">{children}</main>
-          <footer>
-            <Footer />
-          </footer>
+      </Head>
+      <nav className="sticky top-0 h-screen bg-darkblue w-32 border-r border-solid border-darkblue-20">
+        <div className="flex items-center justify-center">
+          <Navbar />
         </div>
-      </body>
-    </html>
+      </nav>
+      <div className="flex-1 flex flex-col bg-darkblue">
+        <Header />
+        <main className="flex-1 px-4 py-8 overflow-y-auto" role="main">
+          {children}
+        </main>
+        <footer role="contentinfo">
+          <Footer />
+        </footer>
+      </div>
+    </div>
   );
 }
