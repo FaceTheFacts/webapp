d7523cbd19fc90786e00971524c0fd366006b8ef
Matias Cardone <matias.cardone@axelspringer.com>
2023-03-29 13:52:27 +0200

    Clean Code. Use of ternary to verify object before access to properties.

--- app/components/PartyTag.tsx
+++ app/components/PartyTag.tsx
@@ -3,9 +3,9 @@ import { getPartyStyle } from '@/app/logic/partycases'
 export default function PartyTag({ party } : PartyTag) {
     const partyTagStyle = getPartyStyle(party)
     const divShape = "rounded-lg max-w-fit mt-2.5"
-    return (
+    return party ? (
         <div className={[partyTagStyle, divShape].join(" ")}>
-            <div className="text-sm text-center font-semibold px-1.75 py-0.875">{party.party_style.display_name}</div>
+            <div className="text-sm text-center font-semibold px-1.75 py-0.875">{party.party_style?.display_name}</div>
         </div>
-    )
+    ) : null;
 }
