467a00723a0c81f32fbf4c0c0f90ed413270816a
Matias Cardone <matias.cardone@axelspringer.com>
2023-04-05 14:09:05 +0200

    Change casing of "alt".

--- app/components/ProfileImage.tsx
+++ app/components/ProfileImage.tsx
@@ -7,7 +7,7 @@ export default function ProfileImage({id} : {id: number}) {
             src={`https://image.facethefacts-api.de/${id}.jpg`}
             width={146}
             height={146}
-            alt={"ProfilBild"}
+            alt={"Profilbild"}
         />
     );
 }
