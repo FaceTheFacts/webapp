12e55100a2882ed80dce528b435f4f0c160ca471
Matias Cardone <matias.cardone@axelspringer.com>
2023-04-05 13:59:52 +0200

    Add PartyTag interface. Remove ternary operator. Apply linting.

--- app/components/PartyTag.tsx
+++ app/components/PartyTag.tsx
@@ -1,11 +1,18 @@
 import { getPartyStyle } from '@/app/logic/partycases'
 
-export default function PartyTag({ party } : PartyTag) {
-    const partyTagStyle = getPartyStyle(party)
-    const divShape = "rounded-lg max-w-fit mt-2.5"
-    return party ? (
-        <div className={[partyTagStyle, divShape].join(" ")}>
-            <div className="text-sm text-center font-semibold px-1.75 py-0.875" data-testid="party-tag">{party.party_style?.display_name}</div>
-        </div>
-    ) : null;
+interface PartyTag {
+	party: Party;
 }
+
+export default function PartyTag({ party }: PartyTag): JSX.Element {
+	const partyTagStyle = getPartyStyle(party);
+	const divShape = "rounded-lg max-w-fit mt-2.5";
+	return (
+		<div className={[partyTagStyle, divShape].join(" ")}>
+			<div className="text-sm text-center font-semibold px-1.75 py-0.875" data-testid="party-tag">
+				{party.party_style.display_name}
+			</div>
+		</div>
+	);
+}
+  
