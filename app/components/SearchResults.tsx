4c62121915078b6de0cacea2d109980ec38390ef
Matias Cardone <matias.cardone@axelspringer.com>
2023-04-13 19:42:27 +0200

    Remove unused code. Return a list.

--- app/components/SearchResults.tsx
+++ app/components/SearchResults.tsx
@@ -6,18 +6,12 @@ export interface Search {
   }
 
 export default function SearchResults({ searchResults }: { searchResults: Search[] }) {
-    // "No Results" only after the search
-    // if (searchResults.length === 0) {
-    //     return <p className='text-white'>No results.</p>
-    // }
-
-    const searchResultItems = searchResults.map((result) => (
-        <li key={result.id}>
-            <Link href={`profile/${result.id}`}>
-                {result.label}
-            </Link>
-        </li>
-    ));
-
-    return <ul className='text-white'>{searchResultItems}</ul>;
+  
+    return (
+      <ul>
+        {searchResults.map((result) => (
+          <li key={result.id}><Link href={`profile/${result.id}`} className='text-white'>{result.label}</Link></li>
+        ))}    
+      </ul>
+    )
 }
