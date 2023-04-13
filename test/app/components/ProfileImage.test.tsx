d1a473888074f4165fe491992a04402c249aca64
Matias Cardone <matias.cardone@axelspringer.com>
2023-04-13 19:40:39 +0200

    Update AltText. Remove log.

--- test/app/components/ProfileImage.test.tsx
+++ test/app/components/ProfileImage.test.tsx
@@ -6,9 +6,8 @@ describe("ProfileImage component", () => {
       const id = 123;
       render(<ProfileImage id={id} />);
       
-      const profileImg = screen.getByAltText("ProfilBild") as HTMLImageElement;
+      const profileImg = screen.getByAltText("Profilbild") as HTMLImageElement;
       const src = profileImg.getAttribute("src");
-      console.log("Generated src attribute:", src);
       expect(src).toMatch(`http://localhost/_next/image?url=https%3A%2F%2Fimage.facethefacts-api.de%2F${id}.jpg&w=384&q=75`);
       expect(profileImg.getAttribute("width")).toBe("146");
       expect(profileImg.getAttribute("height")).toBe("146");
